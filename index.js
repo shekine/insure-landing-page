3f5dd918fbbf9dc685905315338e0010980138cf
Benjamin Benavides <bbenav93@gmail.com>
2020-05-16 03:40:45 -0400

    Initial Commit for Insure Landing Page

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function showDropdown() {
+  let x = document.getElementById("dropdown");
+  if(x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+
+let logo = document.getElementsByClass("logo");
+logo.style.display = "none";
